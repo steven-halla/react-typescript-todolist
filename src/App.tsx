ec1ff7cb35ad74815fb5e4bef467ef5fb44ff849
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-05-11 16:47:18 -0700

    "first"

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import {ITask} from './Interfaces'
 import TodoTask from "./Components/TodoTask";
 
 
-const  App: FC = () => {
-
+const App: FC = () => {
     const [task, setTask] = useState<string>("");
     const [deadline, setDeadline] = useState<number>(0);
     const [todoList, setTodoList] = useState<ITask[]>([]);
@@ -26,6 +25,12 @@ const  App: FC = () => {
         setDeadline(0);
     };
 
+    const completeTask = (taskNameToDelete: string): void => {
+      setTodoList(todoList.filter((task) => {
+        return task.taskName != taskNameToDelete
+        }))
+    };
+
     return (
     <div className="App">
       <div className="header">
@@ -37,7 +42,7 @@ const  App: FC = () => {
       </div>
         <div className="todoList">
             {todoList.map((task: ITask, key: number) => {
-                return <TodoTask key={key} task={task} />;
+                return <TodoTask key={key} task={task} completeTask={completeTask}/>;
             })}
         </div>
     </div>
