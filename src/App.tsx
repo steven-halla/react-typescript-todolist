9a043c5fdc2ffd0995bb4c91d97f9b7e00709f3d
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-05-18 21:57:14 -0700

    " finished "

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,46 @@ import './App.scss';
 import {Header} from "./components/Header";
 import {TodoContextProvider} from "./components/TodoContext";
 import {TodoList} from "./components/TodoList";
-import styled from "styled-components";
+import styled, {ThemeProvider} from "styled-components";
+import {createMuiTheme} from "@material-ui/core";
 
 const StyledApp = styled.div`
-    &.app {
-      display: flex;
-      align-items: center;
-      flex-direction: column;
-      width: 100vw;
-      height: 100vh;
-      font-family: Arial, Helvetica, sans-serif;
-    }
+  &.app {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    font-family: Arial, Helvetica, sans-serif;
+  }
 `;
 
 export const App: FC = () => {
+    const theme = createMuiTheme({
+        palette: {
+            primary: {
+                light: '#757ce8',
+                main: '#3f50b5',
+                dark: '#002884',
+                contrastText: '#fff',
+            },
+            secondary: {
+                light: '#ff7961',
+                main: '#f44336',
+                dark: '#ba000d',
+                contrastText: '#000',
+            },
+        },
+    });
+
     return (
-        <TodoContextProvider>
-            <StyledApp className="app">
-                <Header/>
-                <TodoList/>
-            </StyledApp>
-        </TodoContextProvider>
+        <ThemeProvider theme={theme}>
+            <TodoContextProvider>
+                <StyledApp className="app">
+                    <Header/>
+                    <TodoList/>
+                </StyledApp>
+            </TodoContextProvider>
+        </ThemeProvider>
     );
 }
